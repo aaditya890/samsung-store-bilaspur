ec4857206c56122e2acff94a81d4e850fa6d961d
aaditya890 <mishraaaditya297@gmail.com>
2025-06-26 16:32:01 +0530

    Initial commit - Samsung Store project ready

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,ts}"],
+   theme: {
+     extend: {
+       fontFamily: {
+      samsung: ['SamsungOne', 'SamsungOne'],
+      sharp: ['SamsungSharpSans', 'SamsungSharpSans'],
+      },
+     },
+   },
+   plugins: [],
+ }
